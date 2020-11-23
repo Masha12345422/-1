fd67a087711771c1fe4b5f21793c4f3cdf72590a
soorp@mail.ru <71292607+Masha12345422@users.noreply.github.com>
2020-11-24 01:34:20 +0300

    Add files via upload

---
+++ ЛР6 ТиОПО/lb6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace lb6_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+#endif      
+
+#if !RELEASE
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+#endif
+            Segment segment = new Segment(x1,x2,y1,y2);
+            segment.Segmentlength();
+            segment.Distance();
+            segment.GetInfo();
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+        }
+    }
+}
---
+++ ЛР6 ТиОПО/lb6/Segment.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace lb6_A
+{
+    class Segment
+    {
+        public int  X1 { get; set; }
+        public int X2 { get; set; }
+        public int Y1 { get; set; }
+        public int Y2 { get; set; }
+        public Segment(int x1, int x2, int y1, int y2)
+        {
+            X1 = x1;
+            X2 = x2;
+            Y1 = y1;
+            Y2 = y2;
+        }
+
+        public void Segmentlength()
+        {
+            Console.WriteLine($"\nLength sement = {Math.Sqrt(Math.Pow(X2-X1,2) + Math.Pow(Y2 - Y1, 2))}");
+        }
+
+        public void Distance()
+        {
+            Console.WriteLine($"\nDistance to start of leg: ({X1},{X2})");
+            Console.WriteLine($"Distance to end of line: {X1+Y1},{X2+Y2}");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine("\n*****Segment*****\n\n" +
+                                 "-----------------\n\n"+
+                                 "*****************");
+            Console.WriteLine($"Start of segment: {X1},{X2}");
+            Console.WriteLine($"End of segment: {Y1},{Y2}");
+        }
+
+    }            
+}
